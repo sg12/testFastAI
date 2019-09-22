35d04bbaab9af7a20c6eeb5388e93433615b3ad0
sg12 <sg12@ngs.ru>
2019-09-22 18:30:41 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,13 +12,8 @@ from starlette.staticfiles import StaticFiles
 from fastai.callbacks import *
 import shutil
 from PIL import Image
-import cv2 as cv
-import pandas as pd
+import cv
 import numpy as np
-from fastai.vision import Image as fImage
-from matplotlib import pyplot as plt
-import imutils
-from pathlib import Path
 
 export_file_url = 'https://www.dropbox.com/s/qjs4v9dqgh1dpsu/export_lm_first-2.pkl?dl=1'
 export_file_name = 'export_lm_first-2.pkl'
